5de04e573a4635364710c797f30e1976811e6d62
onrocketup <onro>
2021-09-07 13:59:55 +0300

    cmmit 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
         href="https://pixinvent.com/demo/vuexy-vuejs-admin-dashboard-template/documentation/"
         target="_blank"
       >
-        Template Documentation
+        <h1>Test VP commit vp </h1>
       </b-link> to understand where to go from here and how to use our template.</b-card-text>
     </b-card>
 
