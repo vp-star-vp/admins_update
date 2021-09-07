967878165475fea382b2482dd936d01505e66b3c
vp-star-vp <58979120+vp-star-vp@users.noreply.github.com>
2021-09-07 18:51:10 +0500

    вернул. vp-star-vp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
         target="_blank"
       >
         <h1>Test VP commit vp  2 ///////////// **********</h1>
+        <h1>Test VP commit vp  2 ///////////// **********</h1>
       </b-link> to understand where to go from here and how to use our template.</b-card-text>
     </b-card>
 
